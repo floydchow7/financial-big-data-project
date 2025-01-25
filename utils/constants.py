1eb3936f0ad6ef33651cff010cf310a191475927
floydchow7 <floydchow7@gmail.com>
2025-01-26 00:16:17 +0100

    add comment in constants

--- utils/constants.py
+++ utils/constants.py
@@ -2,14 +2,14 @@
 
 class Constant:
     def __init__(self):
-        self.root_dir = "C:/Users/floyd/Desktop/financial-big-data-project/"
+        self.root_dir = "C:/Users/floyd/Desktop/financial-big-data-project/" # Change this to your root directory
         self.data_dir = self.root_dir + 'data/'
         self.raw_data_dir = self.data_dir + 'raw/'
         self.clean_data_dir = self.data_dir + 'clean/'
         self.processed_data_dir = self.data_dir + 'processed/'
-        self.API_KEY = "Hv6Q2xCqZ1h7mm6v25GrfwBieM4VMxFAQ9h2G8ojZB4SnX6HMQN0dzOWXzIwJxMg"
-        self.API_SECRET = "xTNQ1iICNI2Xu8h5YS8GYSiL7BDd4GQSWtdvIXnZzffsdhUnlmFKWrDDaFSaIwUf"
-        self.FNSPID_data_path = "E:/FNSPID/Stock_news/nasdaq_exteral_data.csv"
+        self.API_KEY = "YOUR_API_KEY" # Change this to your API key
+        self.API_SECRET = "YOUR_API_SECRET" # Change this to your API secret
+        self.FNSPID_data_path = "E:/FNSPID/Stock_news/nasdaq_exteral_data.csv" # Change this to your FNSPID data path
 
 if __name__ == "__main__":
     pass
